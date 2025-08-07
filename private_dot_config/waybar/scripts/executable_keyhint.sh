7f61aa5a20bebf8b0df69027b056b1f800f23137
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-08-07 18:31:57 +0200

    Adjustments to waybar

--- private_dot_config/waybar/scripts/executable_keyhint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python ~/.config/waybar/scripts/keyhint.py | yad --title="EndeavourOS Sway-WM keybindings:" --homogenous --no-buttons --geometry=500x345-15-400 --list --column=Key --column=Description --column=Command
